3d18242e27d920606e3dc22f359d8f8838f4f942
alibek517 <alibek517@users.noreply.github.com>
2026-09-13 11:53:22 +0500

    Disable caching on vercel, bust script caches, force network-first in service worker

--- web/sw.js
+++ web/sw.js
@@ -1,35 +1,15 @@
-const CACHE_NAME = 'ijro-pwa-v2';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './css/style.css',
-  './js/firebase-config.js',
-  './js/chat.js',
-  './js/mayor.js',
-  './js/worker.js',
-  './js/admin.js',
-  './js/app.js',
-  './assets/logo.png',
-  './assets/icon-192.png',
-  './assets/icon-512.png',
-  './assets/apple-touch-icon.png',
-  './manifest.json'
-];
+const CACHE_NAME = 'ijro-pwa-v3';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
+      keys.map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
